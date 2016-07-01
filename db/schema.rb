fc6109e32f8007a96ec37e697ae860eb39a0f204
Catalin Boruga <catalinboruga@yahoo.com>
2016-07-01 20:44:10 +0300

    Add comment resource

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160701170716) do
+ActiveRecord::Schema.define(version: 20160701174210) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "event_id"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "comments", ["event_id"], name: "index_comments_on_event_id", using: :btree
 
   create_table "events", force: :cascade do |t|
     t.datetime "start_date"
@@ -25,4 +34,5 @@ ActiveRecord::Schema.define(version: 20160701170716) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "events"
 end
