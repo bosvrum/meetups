8ae976dc3424bc65ff2d540353d3b709549c09dd
Catalin Boruga <catalinboruga@yahoo.com>
2016-07-01 20:47:12 +0300

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+  root to: 'events#index'
   resources :comments
   resources :events
   # The priority is based upon order of creation: first created -> highest priority.
